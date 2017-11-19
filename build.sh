9dbc999a809f2a916fd89e4f4ed20ee1c6bf5b78
Boris Rybalkin <ribalkin@gmail.com>
2017-11-19 02:11:00 +0000

    phantomjs deps

--- build.sh
+++ build.sh
@@ -80,6 +80,7 @@ git clone git://github.com/Medium/phantomjs.git npm-phantomjs
 cd npm-phantomjs
 git checkout v1.9.20
 sed -i "s/exports.version.*/exports.version = '1.9.20'/g" lib/phantomjs.js
+${BUILD_DIR}/nodejs/bin/npm install phantomjs@1.9.20 || true
 ${BUILD_DIR}/nodejs/bin/node ./install.js
 
 exit 0
