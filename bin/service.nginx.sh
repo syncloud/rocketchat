7020f19bb09fefed9648299c17c49a8a141c0a72
Boris Rybalkin <ribalkin@gmail.com>
2018-01-08 15:30:38 +0000

    fix nginx snap service [skip ci]

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -1,11 +1,31 @@
-#!/bin/bash
+#!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+
 if [[ -z "$1" ]]; then
-    echo "usage $0 config_dir"
+    echo "usage $0 [start|stop]"
     exit 1
 fi
-CONFIG_DIR=$1
-export LD_LIBRARY_PATH=${DIR}/php/lib:${DIR}/postgresql/lib
 
-exec $DIR/php/sbin/php-fpm -y ${CONFIG_DIR}/php-fpm.conf -c ${CONFIG_DIR}/php.ini
+case $1 in
+pre-start)
+    /bin/rm -f ${SNAP_COMMON}/web.socket
+    exec ${DIR}/nginx/sbin/nginx -t -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
+    ;;
+start)
+    exec ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
+    ;;
+post-start)
+    timeout 5 /bin/bash -c 'until [ -S '${SNAP_COMMON}'/web.socket ]; do echo "waiting for ${SNAP_COMMON}/web.socket"; sleep 1; done'
+    ;;
+reload)
+    ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx.conf -s reload -p ${DIR}/nginx
+    ;;
+stop)
+    ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx.conf -s stop -p ${DIR}/nginx
+    ;;
+*)
+    echo "not valid command"
+    exit 1
+    ;;
+esac
