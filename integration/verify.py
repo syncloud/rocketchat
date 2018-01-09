8269837040e1704dc5397343a2c7f45a14bbb4d4
Boris Rybalkin <ribalkin@gmail.com>
2018-01-09 21:01:29 +0000

    debug env file

--- integration/verify.py
+++ integration/verify.py
@@ -67,6 +67,7 @@ def module_teardown(device_host, data_dir, platform_data_dir, app_dir):
     app_log_dir  = join(LOG_DIR, 'rocketcaht_log')
     os.mkdir(app_log_dir )
     run_scp('root@{0}:{1}/log/*.log {2}'.format(device_host, data_dir, app_log_dir), password=LOGS_SSH_PASSWORD, throw=False)
+    run_scp('root@{0}:{1}/config/rocketchat.env {2}'.format(device_host, data_dir, app_log_dir), password=LOGS_SSH_PASSWORD, throw=False)
 
 
 @pytest.fixture(scope='function')
