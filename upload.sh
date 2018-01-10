3ea6d8a2451e2f56bab92bc5904f0db8e6466a3d
Boris Rybalkin <ribalkin@gmail.com>
2018-01-10 08:46:09 +0000

    fix snap upload

--- upload.sh
+++ upload.sh
@@ -2,18 +2,25 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-app=$1
-branch=$2
-build_number=$3
+app=rocketchat
+branch=$1
+build_number=$2
+installer=$3
 bucket=apps.syncloud.org
-arch=$(uname -m)
+ARCH=$(uname -m)
 
 mkdir -p /opt/app
 SAMCMD=/opt/app/sam/bin/sam
 
+FILE_NAME=${app}-${build_number}-${ARCH}.tar.gz
+if [ $installer == "snapd" ]; then
+  ARCH=$(dpkg-architecture -q DEB_HOST_ARCH)
+  FILE_NAME=${app}_${build_number}_${ARCH}.snap
+fi
+
 if [ "${branch}" == "master" ] || [ "${branch}" == "stable" ] ; then
-  
-  s3cmd put ${app}-${build_number}-${arch}.tar.gz s3://${bucket}/apps/${app}-${build_number}-${arch}.tar.gz
+
+  s3cmd put $FILE_NAME s3://${bucket}/apps/$FILE_NAME
   
   if [ "${branch}" == "stable" ]; then
     branch=rc
