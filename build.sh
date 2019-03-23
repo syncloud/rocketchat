143f541f758ec70b118cc02daf3dfe50968a4e63
Boris Rybalkin <ribalkin@gmail.com>
2019-03-23 08:58:46 +0000

    v0.74.3

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ export TMPDIR=/tmp
 export TMP=/tmp
 
 NAME=$1
-ROCKETCHAT_VERSION=0.71.1
+ROCKETCHAT_VERSION=0.74.3
 COIN_CACHE_DIR=${DIR}/coin.cache
 ARCH=$(uname -m)
 SNAP_ARCH=$(dpkg --print-architecture)
