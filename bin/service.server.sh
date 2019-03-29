48d005f0a3a757f558b833dcf4d2924f9a8403f5
Boris Rybalkin <ribalkin@gmail.com>
2019-03-29 18:23:00 +0000

    try fixing permissions

--- bin/service.server.sh
+++ bin/service.server.sh
@@ -11,12 +11,12 @@ fi
 
 case $1 in
 pre-start)
-    #timeout 300 /bin/bash -c 'until echo > /dev/tcp/localhost/'$MONGO_PORT'; do sleep 1; done'
-    #timeout 100 /bin/bash -c 'until [ -S '$MONGO_SOCKET_FILE' ]; do echo "waiting for ${MONGO_SOCKET_FILE}"; sleep 1; done'
+    timeout 300 /bin/bash -c 'until echo > /dev/tcp/localhost/'$MONGO_PORT'; do sleep 1; done'
+    timeout 100 /bin/bash -c 'until [ -S '$MONGO_SOCKET_FILE' ]; do echo "waiting for ${MONGO_SOCKET_FILE}"; sleep 1; done'
     ;;
 start)
     echo "MONGO_URL: $MONGO_URL" | logger -t rocketchat
-    #exec ${DIR}/nodejs/bin/node ${DIR}/bundle/main.js 2>&1
+    exec ${DIR}/nodejs/bin/node ${DIR}/bundle/main.js 2>&1
     ;;
 *)
     echo "not valid command"
