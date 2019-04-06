2a40be68de088698f02e0dcb34bd17aad81baf43
Boris Rybalkin <ribalkin@gmail.com>
2019-04-06 23:40:05 +0100

    try single install again and remove pre-compiled libs

--- build.sh
+++ build.sh
@@ -89,9 +89,10 @@ sed -i "s/exports.version.*/exports.version = '1.9.20'/g" lib/phantomjs.js
 ${BUILD_DIR}/nodejs/bin/npm install --unsafe-perm --production -g
 
 cd ${BUILD_DIR}/bundle/programs/server
-SHARP_DIST_BASE_URL="http://artifact.syncloud.org/3rdparty/" ${BUILD_DIR}/nodejs/bin/npm install sharp@0.21.0 --unsafe-perm --production -g
-sed -i '/"sharp": "^0.21.0"/d' package.json
-${BUILD_DIR}/nodejs/bin/npm install --unsafe-perm --production
+#SHARP_DIST_BASE_URL="http://artifact.syncloud.org/3rdparty/" ${BUILD_DIR}/nodejs/bin/npm install sharp@0.21.0 --unsafe-perm --production -g
+#sed -i '/"sharp": "^0.21.0"/d' package.json
+rm -rf npm/node_modules/sharp/vendor
+SHARP_DIST_BASE_URL="http://artifact.syncloud.org/3rdparty/" ${BUILD_DIR}/nodejs/bin/npm install --unsafe-perm --production
 
 mkdir ${DIR}/build/${NAME}/META
 echo ${NAME} >> ${DIR}/build/${NAME}/META/app
