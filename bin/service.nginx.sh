e22b32349ef30d588e6c62ac394a83ac10ef358e
Boris Rybalkin <ribalkin@gmail.com>
2019-04-09 22:01:30 +0100

    extend timeout more for arm testing

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -11,7 +11,7 @@ fi
 
 case $1 in
 pre-start)
-    timeout 1200 /bin/bash -c 'until echo > /dev/tcp/localhost/'$PORT'; do sleep 1; done'
+    timeout 2000 /bin/bash -c 'until echo > /dev/tcp/localhost/'$PORT'; do sleep 1; done'
     /bin/rm -f ${SNAP_COMMON}/web.socket
     exec ${DIR}/nginx/sbin/nginx -t -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     ;;
