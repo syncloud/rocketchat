ee80d79555505ac3157fe3a285b17f4d894fd189
Boris Rybalkin <ribalkin@gmail.com>
2019-06-03 10:01:05 +0100

    fix start retry

--- bin/service.server.sh
+++ bin/service.server.sh
@@ -13,12 +13,12 @@ export LD_LIBRARY_PATH=${DIR}/lib
 
 case $1 in
 start)
-    bash -c "for i in `seq 1 30`; do exec ${DIR}/mongodb/bin/mongo localhost/rocketchat ${SNAP_COMMON}/config/mongo.configure.js && s=$$? && break || s=$$?; echo \"Tried $$i times. Waiting 5 secs...\"; sleep 5; done; (exit $$s)"
+    bash -c "for i in \$(seq 1 30); do ${DIR}/mongodb/bin/mongo localhost/rocketchat ${SNAP_COMMON}/config/mongo.configure.js && s=\$? && break || s=\$?; echo \"Tried \$i times. Waiting 5 secs...\"; sleep 5; done; (exit \$s)"
 
     echo "MONGO_URL: $MONGO_URL" | logger -t rocketchat
     echo "MONGO_OPLOG_URL: $MONGO_OPLOG_URL" | logger -t rocketchat
     #exec ${DIR}/nodejs/bin/node ${DIR}/bundle/main.js 2>&1
-    bash -c "for i in `seq 1 30`; do ${DIR}/nodejs/bin/node ${DIR}/bundle/main.js && s=$$? && break || s=$$?; echo "Tried $$i times. Waiting 5 secs..."; sleep 5; done; (exit $$s)"
+    bash -c "for i in \$(seq 1 30); do ${DIR}/nodejs/bin/node ${DIR}/bundle/main.js && s=\$? && break || s=\$?; echo \"Tried \$i times. Waiting 5 secs...\"; sleep 5; done; (exit \$s)"
     ;;
 *)
     echo "not valid command"
