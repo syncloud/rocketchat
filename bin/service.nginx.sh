2c9e15f9736497f1cc5af772377617fb9ffd2262
Boris Rybalkin <ribalkin@gmail.com>
2019-09-11 07:29:19 +0100

    remove pre start

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -10,12 +10,10 @@ fi
 . $SNAP_COMMON/config/rocketchat.env
 
 case $1 in
-pre-start)
-		   timeout 1000 /bin/bash -c 'until echo > /dev/tcp/localhost/'$PORT'; do sleep 5; done'
-		   /bin/rm -f ${SNAP_COMMON}/web.socket
-		   exec ${DIR}/nginx/sbin/nginx -t -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
-		   ;;
 start)
+    timeout 1000 /bin/bash -c 'until echo > /dev/tcp/localhost/'$PORT'; do sleep 5; done'
+		   /bin/rm -f ${SNAP_COMMON}/web.socket
+		   ${DIR}/nginx/sbin/nginx -t -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     exec ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     ;;
 post-start)
