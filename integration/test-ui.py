7051420e3d888f0dc80f2c1d4903a2e61654720a
Boris Rybalkin <ribalkin@gmail.com>
2020-12-08 18:12:10 +0000

    fix screenshot artifacts

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -3,6 +3,7 @@ import shutil
 from os.path import dirname, join, exists
 import time
 import pytest
+from subprocess import check_output
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -16,25 +17,36 @@ from syncloudlib.integration.screenshots import screenshots
 
 
 DIR = dirname(__file__)
-screenshot_dir = join(DIR, 'screenshot')
+TMP_DIR = '/tmp/syncloud/ui'
+
+
+@pytest.fixture(scope="session")
+def module_setup(request, device, artifact_dir, ui_mode):
+    def module_teardown():
+        device.activated()
+        device.run_ssh('mkdir -p {0}'.format(TMP_DIR), throw=False)
+        device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
+        device.run_ssh('cp /var/log/syslog {0}/syslog.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
+        device.scp_from_device('{0}/*'.format(TMP_DIR), join(artifact_dir, 'log'))
+        check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
+
+    request.addfinalizer(module_teardown)
+
 
 def test_start(app, device_host):
-    if exists(screenshot_dir):
-        shutil.rmtree(screenshot_dir)
-    os.mkdir(screenshot_dir)
-
     add_host_alias(app, device_host)
-    
-def test_index(driver, app_domain):
+
+
+def test_index(driver, app_domain, ui_mode, screenshot_dir):
 
     driver.get("https://{0}".format(app_domain))
     time.sleep(10)
     print(driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []'))
-    screenshots(driver, screenshot_dir, 'index')
+    screenshots(driver, screenshot_dir, 'index-' + ui_mode)
     print(driver.page_source.encode('utf-8'))
 
 
-def test_login(driver, app_domain, device_user, device_password):
+def test_login(driver, app_domain, device_user, device_password, ui_mode, screenshot_dir):
 
     wait_driver = WebDriverWait(driver, 120)
     wait_driver.until(EC.element_to_be_clickable((By.ID, 'emailOrUsername')))
@@ -43,85 +55,85 @@ def test_login(driver, app_domain, device_user, device_password):
     user.send_keys(device_user)
     password = driver.find_element_by_id("pass")
     password.send_keys(device_password)
-    screenshots(driver, screenshot_dir, 'login')
+    screenshots(driver, screenshot_dir, 'login-' + ui_mode)
 
     password.send_keys(Keys.RETURN)
-    screenshots(driver, screenshot_dir, 'login_progress')
+    screenshots(driver, screenshot_dir, 'login_progress-' + ui_mode)
     time.sleep(20)
-    screenshots(driver, screenshot_dir, 'setup')
+    screenshots(driver, screenshot_dir, 'setup-' + ui_mode)
      
      
-def test_setup(driver, app_domain):
+def test_setup(driver, ui_mode, screenshot_dir):
 
-    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Organization_Type')
+    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Organization_Type-' + ui_mode)
     select = Select(driver.find_element_by_name('Organization_Type'))
     select.select_by_visible_text('Community')
     
     time.sleep(2)
-    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Organization_Name')
+    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Organization_Name-' + ui_mode)
     anme = driver.find_element_by_name('Organization_Name')
     anme.send_keys('Syncloud')
 
     time.sleep(2)
-    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Industry')
+    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Industry-' + ui_mode)
     select = Select(driver.find_element_by_name('Industry'))
     select.select_by_visible_text('Technology Provider')
 
     time.sleep(2)
-    screenshots(driver, screenshot_dir, 'setup-wizard-Size')
+    screenshots(driver, screenshot_dir, 'setup-wizard-Size-' + ui_mode)
     select = Select(driver.find_element_by_name('Size'))
     select.select_by_visible_text('4000 or more people')
 
     time.sleep(2)
-    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Country')
+    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Country-' + ui_mode)
     select = Select(driver.find_element_by_name('Country'))
     select.select_by_visible_text('United Kingdom')
 
     time.sleep(2)
-    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Website')
+    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Website-' + ui_mode)
     website = driver.find_element_by_name('Website')
     website.send_keys('syncloud.org')
 
     time.sleep(2)
-    screenshots(driver, screenshot_dir, 'setup-wizard-step-1')
+    screenshots(driver, screenshot_dir, 'setup-wizard-step-1-' + ui_mode)
     driver.find_element_by_css_selector('.setup-wizard-forms__footer-next').click()
     time.sleep(10)
 
-    screenshots(driver, screenshot_dir, 'setup-wizard-debug-site-name')
+    screenshots(driver, screenshot_dir, 'setup-wizard-debug-site-name-' + ui_mode)
     site = driver.find_element_by_name('Site_Name')
     site.send_keys('Syncloud')
 
     time.sleep(2)
-    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Server_Type')
+    screenshots(driver, screenshot_dir, 'setup-wizard-debug-Server_Type-' + ui_mode)
     select = Select(driver.find_element_by_name('Server_Type'))
     select.select_by_visible_text('Private Team')
     
-    screenshots(driver, screenshot_dir, 'setup-wizard-step-2')
+    screenshots(driver, screenshot_dir, 'setup-wizard-step-2-' + ui_mode)
 
     driver.find_element_by_css_selector('.setup-wizard-forms__footer-next').click()
     time.sleep(10)
 
-    screenshots(driver, screenshot_dir, 'setup-wizard-step-3')
+    screenshots(driver, screenshot_dir, 'setup-wizard-step-3-' + ui_mode)
     driver.find_element_by_css_selector('.setup-wizard-forms__content-register-radio-text').click()
     driver.find_element_by_css_selector('.setup-wizard-forms__footer-next').click()
     time.sleep(10)
 
 
-def test_welcome(driver):
+def test_welcome(driver, ui_mode, screenshot_dir):
     
-    screenshots(driver, screenshot_dir, 'welcome')
+    screenshots(driver, screenshot_dir, 'welcome-' + ui_mode)
     driver.find_element_by_css_selector('.js-finish').click()
     time.sleep(30)
 
 
-def test_main(driver):
-    screenshots(driver, screenshot_dir, 'main')
+def test_main(driver, ui_mode, screenshot_dir):
+    screenshots(driver, screenshot_dir, 'main-' + ui_mode)
 
 
-def test_profile(driver, app_domain, device_password):
+def test_profile(driver, app_domain, device_password, ui_mode, screenshot_dir):
     driver.get("https://{0}/account/profile".format(app_domain))
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'profile')
+    screenshots(driver, screenshot_dir, 'profile-' + ui_mode)
 
     profile_file = driver.find_element_by_css_selector('input[type="file"]')
     profile_file.send_keys(join(DIR, 'images', 'profile.jpeg'))
@@ -135,7 +147,7 @@ def test_profile(driver, app_domain, device_password):
    
     time.sleep(2)
     
-    screenshots(driver, screenshot_dir, 'profile-new-name')
+    screenshots(driver, screenshot_dir, 'profile-new-name-' + ui_mode)
 
     save = driver.find_element_by_name('send')
     save.click()
@@ -148,12 +160,12 @@ def test_profile(driver, app_domain, device_password):
     
     time.sleep(10)
 
-    screenshots(driver, screenshot_dir, 'profile-new-picture')
+    screenshots(driver, screenshot_dir, 'profile-new-picture-' + ui_mode)
 
 
-def test_channel(driver, app_domain):
+def test_channel(driver, app_domain, ui_mode, screenshot_dir):
     driver.get("https://{0}/channel/general".format(app_domain))
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'channel')
+    screenshots(driver, screenshot_dir, 'channel-' + ui_mode)
 
     
