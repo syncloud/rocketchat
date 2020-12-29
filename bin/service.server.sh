d961a10741e3486dc2c259756d6c99affdf13a26
Boris Rybalkin <ribalkin@gmail.com>
2020-12-29 22:00:00 +0000

    mongo shell test

--- bin/service.server.sh
+++ bin/service.server.sh
@@ -13,8 +13,20 @@ export LD_LIBRARY_PATH=${DIR}/mongodb/lib
 
 case $1 in
 start)
-    bash -c "for i in \$(seq 1 30); do ${DIR}/mongodb/bin/mongo.sh localhost/rocketchat ${SNAP_COMMON}/config/mongo.configure.js && s=\$? && break || s=\$?; echo \"Tried \$i times. Waiting 5 secs...\"; sleep 5; done; (exit \$s)"
-
+    started=0
+    for i in $(seq 1 30); do
+	set +e
+        ${DIR}/mongodb/bin/mongo.sh localhost/rocketchat ${SNAP_COMMON}/config/mongo.configure.js
+	if [[ $? == 0 ]]; then
+	    started=1
+	    break
+	fi
+	echo "Tried $i times. Waiting 5 secs..."
+	sleep 5
+    done
+    if [[ $started == 0 ]]; then
+        exit 1
+    fi
     echo "MONGO_URL: $MONGO_URL" | logger -t rocketchat
     echo "MONGO_OPLOG_URL: $MONGO_OPLOG_URL" | logger -t rocketchat
     #exec ${DIR}/nodejs/bin/node ${DIR}/bundle/main.js 2>&1
