116224c6bf19791c401e805991d8ca0d596f1de7
Boris Rybalkin <ribalkin@gmail.com>
2021-12-24 10:21:30 +0000

    fix artifacts

--- package.sh
+++ package.sh
@@ -29,3 +29,5 @@ echo "- ${ARCH}" >> ${SNAP_DIR}/meta/snap.yaml
 PACKAGE=${NAME}_${VERSION}_${ARCH}.snap
 echo ${PACKAGE} > ${DIR}/package.name
 mksquashfs ${SNAP_DIR} ${DIR}/${PACKAGE} -noappend -comp xz -no-xattrs -all-root
+mkdir ${DIR}/artifact
+cp ${DIR}/${PACKAGE} ${DIR}/artifact
