5d0dfade18ea1d2a5d041d2450d6bff36f2f0648
Boris Rybalkin <ribalkin@gmail.com>
2021-12-24 22:03:02 +0000

    refactor server wait

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -9,10 +9,27 @@ fi
 
 . $SNAP_COMMON/config/rocketchat.env
 
+function wait_for_server() {
+    echo "waiting for server"
+    set +e
+    for i in $(seq 1 30); do
+      echo > /dev/tcp/localhost/'$PORT'
+      if [[ $? == 0 ]]; then
+        started=1
+        break
+      fi
+      echo "Tried $i times. Waiting 5 secs..."
+      sleep 5
+    done
+    set -e
+    if [[ $started == 0 ]]; then
+        exit 1
+    fi
+}
+
 case $1 in
 start)
-    echo "waiting for server"
-    timeout 1000 /bin/bash -c 'until echo > /dev/tcp/localhost/'$PORT'; do sleep 5; done'
+    wait_for_server
 	  /bin/rm -f ${SNAP_COMMON}/web.socket
     exec ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     ;;
