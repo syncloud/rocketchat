0a204a283ff03ae0b09718402c44f5a2c7fbeee9
Boris Rybalkin <ribalkin@gmail.com>
2021-12-30 10:57:37 +0000

    ui logs

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -14,9 +14,9 @@ def module_setup(request, device, artifact_dir, ui_mode):
     def module_teardown():
         device.activated()
         device.run_ssh('mkdir -p {0}'.format(TMP_DIR), throw=False)
-        device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
-        device.run_ssh('cp /var/log/syslog {0}/syslog.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
-        device.scp_from_device('{0}/*'.format(TMP_DIR), join(artifact_dir, 'log'))
+        device.run_ssh('journalctl > {0}/journalctl.log'.format(TMP_DIR, ui_mode), throw=False)
+        device.run_ssh('cp /var/log/syslog {0}/syslog.log'.format(TMP_DIR, ui_mode), throw=False)
+        device.scp_from_device('{0}/*'.format(TMP_DIR), join(artifact_dir, ui_mode))
         check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
 
     request.addfinalizer(module_teardown)
