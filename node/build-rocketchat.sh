e6ab0287ab1459914376b82fe754ba2890a7b3e2
Boris Rybalkin <ribalkin@gmail.com>
2022-02-11 08:32:52 +0000

    libvips

--- node/build-rocketchat.sh
+++ node/build-rocketchat.sh
@@ -5,17 +5,17 @@ ROCKETCHAT_VERSION=$1
 BUILD_DIR=${DIR}
 
 apt update
-apt -y install wget patch libglib2.0-0 python build-essential pkg-config glib2.0-dev libexpat1-dev libtool autoconf g++ build-essential
+apt -y install wget patch libglib2.0-0 python build-essential pkg-config glib2.0-dev libexpat1-dev libtool autoconf g++ build-essential libvips-dev
 
-cd $BUILD_DIR
-wget https://github.com/libvips/libvips/releases/download/v8.12.1/vips-8.12.1.tar.gz
-tar xf vips-*
-rm vips-*.tar.gz
-cd vips-*
-./configure
-make
-make install
-rm -rf
+#cd $BUILD_DIR
+#wget https://github.com/libvips/libvips/releases/download/v8.12.1/vips-8.12.1.tar.gz
+#tar xf vips-*
+#rm vips-*.tar.gz
+#cd vips-*
+#./configure
+#make
+#make install
+#rm -rf
 #cp /usr/local/lib/libvips*.so* $BUILD_DIR/nodejs/usr/lib/*-linux-gnu*/
 #ls -la $BUILD_DIR/nodejs/usr/lib/*-linux-gnu*/
 
