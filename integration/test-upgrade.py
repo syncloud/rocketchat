e553a02f8bd8ec609446a3c47c4f08da680a6bd6
Boris Rybalkin <ribalkin@gmail.com>
2022-02-20 17:08:24 +0000

    login

--- integration/test-upgrade.py
+++ integration/test-upgrade.py
@@ -68,8 +68,8 @@ def test_upgrade(device, arch, selenium, device_user, device_password, device_ho
         '{0}/mongodb/bin/mongo.sh /mongodb.config.dump.js > {1}/mongo.config.refresh.dump.log'.format(app_dir, TMP_DIR),
         throw=False)
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
-    login(selenium, device_user, device_password)
-  
+    #login(selenium, device_user, device_password)
+    selenium.open_app()
     selenium.driver.get("https://{0}/channel/general".format(app_domain))
     #v3 selenium.find_by_xpath("//div[text()='Start of conversation']")
     selenium.find_by_xpath("//*[text()='Start of conversation']")
