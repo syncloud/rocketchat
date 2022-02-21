f1ed1d108a547cdd492c67c226c911e70c5abb18
Boris Rybalkin <ribalkin@gmail.com>
2022-02-21 11:49:14 +0000

    login after upgrade if needed

--- integration/lib.py
+++ integration/lib.py
@@ -4,10 +4,12 @@ from selenium.webdriver.common.keys import Keys
 def login(selenium, device_user, device_password):
     selenium.open_app()
     selenium.screenshot('index')
-    selenium.find_by_id("emailOrUsername").send_keys(device_user)
-    password = selenium.find_by_id("pass")
-    password.send_keys(device_password)
-    selenium.screenshot('login')
-    password.send_keys(Keys.RETURN)
-    selenium.screenshot('login_progress')
-
+    try:
+        selenium.find_by_id("emailOrUsername").send_keys(device_user)
+        password = selenium.find_by_id("pass")
+        password.send_keys(device_password)
+        selenium.screenshot('login')
+        password.send_keys(Keys.RETURN)
+        selenium.screenshot('login_progress')
+    except Exception as e:
+        print("no login required: {0}".format(str(e)))
