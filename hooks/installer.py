7fba4a2f317462229f053aa39fa212e3ca2c9ec1
Boris Rybalkin <ribalkin@gmail.com>
2022-03-09 06:45:00 +0000

    fix version

--- hooks/installer.py
+++ hooks/installer.py
@@ -17,7 +17,7 @@ USER_NAME = 'rocketchat'
 PORT = 3000
 MONGODB_PORT = 27017
 REST_URL = "http://localhost:{0}/api/v1".format(PORT)
-SUPPORTED_MAJOR_VERSION = '1.3'
+SUPPORTED_MAJOR_VERSION = '2.0'
 
 class Installer:
     def __init__(self):
