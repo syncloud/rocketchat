7fc6872a6105e62937ed05567fd26abab405094e
Boris Rybalkin <ribalkin@gmail.com>
2022-09-09 19:40:54 +0100

    cleanup

--- node/build-rocketchat.sh
+++ node/build-rocketchat.sh
@@ -40,3 +40,8 @@ cd programs/server
 CXX=g++-4.8 
 CC=gcc-4.8
 npm install --unsafe-perm --production
+
+rustup self uninstall
+apt -y remove patch libglib2.0-0 python build-essential pkg-config glib2.0-dev libexpat1-dev libtool autoconf g++ build-essential cmake
+apt autoclean
+apt autoremove
