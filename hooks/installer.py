12229f637d911dcb4922cec1f13cf901ec8fd76f
Boris Rybalkin <ribalkin@gmail.com>
2022-09-18 17:23:55 +0100

    clesnup

--- hooks/installer.py
+++ hooks/installer.py
@@ -90,7 +90,7 @@ class Installer:
             self._upgrade()
         else:
             storage.init_storage(APP_NAME, USER_NAME)
-            #self._install()
+ 
 
     def check_major_version(self):
         if path.isfile(self.version_old_file):
@@ -119,82 +119,8 @@ class Installer:
             self.log.error("upgrade error: " + e.output.decode())
             raise e
 
-    def _install(self):
-        self.log.info('configure install')
-        password = uuid.uuid4().hex
-        session = requests_unixsocket.Session()
-        response = session.post("{0}/users.register".format(self.base_url),
-                                json={
-                                     "username": "installer",
-                                     "email": "installer@example.com",
-                                     "pass": password,
-                                     "name": "installer"})
-
-        result = json.loads(response.text)
-        if not result['success']:
-            self.log.info('response: {0}'.format(response.text.encode("utf-8")))
-            raise Exception('cannot create install account')
-
-        self.log.info('install account has been created')
-
-        response = session.post("{0}/login".format(self.base_url), json={"username": "installer", "password": password})
-        result = json.loads(response.text)
-        if not result['status'] == 'success':
-            self.log.error(response.text.encode("utf-8"))
-            self.log.info(result['status'])
-            raise Exception('unable to login under install user')
-
-        auth_token = result['data']['authToken']
-        user_id = result['data']['userId']
-        self.log.info('install account token extracted')
-
-        self.update_setting('LDAP_Enable', True, auth_token, user_id)
-        self.update_setting('LDAP_Server_Type', '', auth_token, user_id)
-        self.update_setting('LDAP_Host', 'localhost', auth_token, user_id)
-        self.update_setting('LDAP_BaseDN', 'dc=syncloud,dc=org', auth_token, user_id)
-        self.update_setting('LDAP_Authentication', True, auth_token, user_id)
-        self.update_setting('LDAP_Authentication_UserDN', 'dc=syncloud,dc=org', auth_token, user_id)
-        self.update_setting('LDAP_Authentication_Password', 'syncloud', auth_token, user_id)
-        self.update_setting('LDAP_User_Search_Filter', '(objectclass=inetOrgPerson)', auth_token, user_id)
-        self.update_setting('LDAP_User_Search_Field', 'cn', auth_token, user_id)
-        self.update_setting('LDAP_Username_Field', 'cn', auth_token, user_id)
-        self.update_setting('Accounts_RegistrationForm', 'Disabled', auth_token, user_id)
-        self.update_setting('Accounts_TwoFactorAuthentication_Enabled', False, auth_token, user_id)
-        self.update_setting('Show_Setup_Wizard', 'completed', auth_token, user_id)
-        self.update_setting('Accounts_Send_Email_When_Activating', False, auth_token, user_id)
-        self.update_setting('Accounts_Send_Email_When_Deactivating', False, auth_token, user_id)
-        self.update_setting('FileUpload_Storage_Type', 'FileSystem', auth_token, user_id)
-
-        app_storage_dir = storage.init_storage(APP_NAME, USER_NAME)
-
-        self.update_setting('FileUpload_FileSystemPath', app_storage_dir, auth_token, user_id)
-
-        response = session.post("{0}/users.delete".format(self.base_url),
-                                 headers={"X-Auth-Token": auth_token, "X-User-Id": user_id},
-                                 json={"userId": user_id})
-        result = json.loads(response.text)
-        if not result['success']:
-            self.log.error(response.text.encode("utf-8"))
-            raise Exception('unable to delete install user')
-
-        with open(self.install_file, 'w') as f:
-            f.write('installed\n')
-
-    def update_setting(self, name, value, auth_token, user_id):
-        # throttle api requests
-        time.sleep(5)
-        session = requests_unixsocket.Session()
-        response = session.post("{0}/settings/{1}".format(self.base_url, name),
-                                 headers={"X-Auth-Token": auth_token, "X-User-Id": user_id},
-                                 json={"value": value})
-        result = json.loads(response.text)
-        if not result['success']:
-            self.log.info('cannot update setting: {0}'.format(name))
-            self.log.info('response: {0}'.format(response.text.encode("utf-8")))
-            raise Exception('unable to update settings')
-
+    
     def prepare_storage(self):
         app_storage_dir = storage.init_storage(APP_NAME, USER_NAME)
         return app_storage_dir
-
 
