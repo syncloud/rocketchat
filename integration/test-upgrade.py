05f8735da3026160cd540ec876b8044227086438
Boris Rybalkin <ribalkin@gmail.com>
2022-09-24 14:34:34 +0100

    import

--- integration/test-upgrade.py
+++ integration/test-upgrade.py
@@ -2,7 +2,7 @@ import pytest
 from subprocess import check_output
 from syncloudlib.integration.hosts import add_host_alias
 from syncloudlib.integration.installer import local_install
-from integration.lib import login_3
+from integration.lib import login_3, login_4
 from syncloudlib.http import wait_for_rest
 from selenium.webdriver.common.keys import Keys
 import requests
@@ -52,8 +52,8 @@ def test_upgrade(device, selenium, device_user, device_password, device_host, ap
         '{0}/mongodb/bin/mongo.sh /mongodb.config.dump.js > {1}/mongo.config.refresh.dump.log'.format(app_dir, TMP_DIR),
         throw=False)
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
-    selenium.driver.get("https://{0}/channel/general".format(app_domain))
     login_4(selenium, device_user, device_password)
+    selenium.driver.get("https://{0}/channel/general".format(app_domain))
     selenium.find_by_xpath("//*[text()='Start of conversation']")
     selenium.find_by_xpath("//p[contains(.,'test message')]")
     selenium.screenshot('refresh-channel')
