350c60d5603f2185b9194042911207461cc12052
Boris Rybalkin <ribalkin@gmail.com>
2024-06-21 21:32:51 +0100

    fix login

--- test/lib.py
+++ test/lib.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -18,8 +19,8 @@ def login_4(selenium, device_user, device_password):
 def login_5(selenium, device_user, device_password):
     selenium.open_app()
     selenium.screenshot('index')
-    selenium.find_by_id("username").send_keys(device_user)
-    password = selenium.find_by_id("password")
+    selenium.find_by(By.XPATH, "//input[@name='username']").send_keys(device_user)
+    password = selenium.find_by(By.XPATH, "//input[@name='password']")
     password.send_keys(device_password)
     selenium.screenshot('login')
     password.send_keys(Keys.RETURN)
