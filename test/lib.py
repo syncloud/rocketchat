21f733e6562f3771c266f874cf4fbdfc2c5cc201
Boris Rybalkin <ribalkin@gmail.com>
2024-09-30 07:54:54 +0100

    sso fix

--- test/lib.py
+++ test/lib.py
@@ -32,16 +32,22 @@ def login_5(selenium, device_user, device_password):
 def login_6(selenium, device_user, device_password):
     selenium.open_app()
     selenium.screenshot('index')
-    selenium.find_by(By.XPATH, "//span[.='Login with Syncloud']").click()  
-    selenium.find_by(By.XPATH, "//input[@name='usernameOrEmail']").send_keys(device_user)
-    password = selenium.find_by(By.XPATH, "//input[@name='password']")
-    password.send_keys(device_password)
-    selenium.screenshot('login')
-    password.send_keys(Keys.RETURN)
-    selenium.screenshot('login_progress')
+    selenium.find_by(By.XPATH, "//span[.='Login with Syncloud']").click()
+
+    login_sso(device_user, device_password) 
+    
     selenium.find_by_xpath("//button[@title='Search']")
     selenium.screenshot('main')
     selenium.find_by_xpath("//button[@title='Administration']").click()
     selenium.find_by_xpath("//button[@title='Workspace']")
     selenium.screenshot('admin')
+
+def login_sso(user, password):
+    selenium.find_by(By.ID, "username-textfield").send_keys(duser)
+    password = selenium.find_by(By.ID, "password-textfield")
+    password.send_keys(password)
+    selenium.screenshot('login')
+    #password.send_keys(Keys.RETURN)
+    selenium.find_by(By.ID, "sign-in-button").click()
+    selenium.find_by(By.ID, "accept-button").click()
 
