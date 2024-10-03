fd0f7ebdb736e4d285e885c03590f6f7427325e0
Boris Rybalkin <ribalkin@gmail.com>
2024-10-03 22:12:07 +0100

    fix

--- test/ui.py
+++ test/ui.py
@@ -105,4 +105,3 @@ def test_profile(selenium, app_domain):
 #    selenium.driver.get("https://{0}/channel/general".format(app_domain))
 #    selenium.find_by_xpath("//*[text()='Start of conversation']")
 #    selenium.screenshot('channel')
-l')
