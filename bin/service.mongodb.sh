7afdaab1ac5b0b66d3b67c58d7804ad04d176f6f
Boris Rybalkin <ribalkin@gmail.com>
2024-10-03 20:46:36 +0100

    fix

--- bin/service.mongodb.sh
+++ bin/service.mongodb.sh
@@ -3,5 +3,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 ulimit -n 64000
-exec ${DIR}/mongodb/bin/mongod.sh --config /snap/rocketchat/current/config/mongodb.conf/rocketchat.env
+exec ${DIR}/mongodb/bin/mongod.sh --config /snap/rocketchat/current/config/mongodb.conf
 
