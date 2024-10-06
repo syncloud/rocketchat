6493dbc064c4c023432d79102a7b0b486c19bee8
Boris Rybalkin <ribalkin@gmail.com>
2024-10-06 12:16:42 +0100

    app dir

--- test/upgrade.py
+++ test/upgrade.py
@@ -39,7 +39,7 @@ def test_login(selenium, device_user, device_password):
     login_6(selenium, device_user, device_password)
 
 
-def test_upgrade(device, selenium, app_domain):
+def test_upgrade(device, selenium, app_domain, app_dir):
 
     send_message(selenium, app_domain)
     read_message(selenium, app_domain)
