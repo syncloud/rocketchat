33ae27ebaf4abb15a7ed8d208664e99c290e0f86
Boris Rybalkin <ribalkin@gmail.com>
2024-10-06 20:55:45 +0100

    fix wait

--- test/lib.py
+++ test/lib.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from syncloudlib.http import wait_for_rest
 
 
 def login_6(selenium, device_user, device_password):
