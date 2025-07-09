6efa58288a6c77a2f8d03eb439c275efcfcb3ba5
Boris Rybalkin <ribalkin@gmail.com>
2025-07-09 21:32:24 +0100

    complete registration

--- test/upgrade.py
+++ test/upgrade.py
@@ -64,6 +64,7 @@ def test_upgrade(device, selenium, device_user, device_password, device_host, ap
     #selenium.find_by_xpath("//button[@title='User menu']")
     selenium.screenshot('login-sso-3-done')
     register_7(selenium)
+    disable_registration_7(selenium, app_domain, device)
 
     #read_message(selenium, app_domain)
     selenium.find_by(By.XPATH, "//div[.='general']").click()
